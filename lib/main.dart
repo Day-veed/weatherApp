002de8c814c40f9e030d9c860cab731ea63bb2ce
Day-veed <davidadekunle9@gmail.com>
2021-06-25 22:56:57 +0100

    stressful update

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,45 @@
 //import 'dart:html';
+//import 'dart:developer';
+//import 'dart:ffi';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 //import 'package:shared_preferences/shared_preferences.dart';
+//import 'package:weatherapp/user.dart';
 
 void main() {
-  runApp(new MaterialApp(
+  runApp( MaterialApp(
     home: HomePage(),
     routes: <String, WidgetBuilder> {
       "/currentweatherpage": (BuildContext context) => new CurrentWeatherPage()
     }
   ));
 }
-String name;
-class HomePage extends StatelessWidget {
+
+
+
+
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+  
+class HomePageState extends State<HomePage> {
   // This widget is the root of your application.
+
+  final username_controller = TextEditingController();
+
+    SharedPreferences logindata;
+    bool newUser;
+
+    @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,13 +53,25 @@ class HomePage extends StatelessWidget {
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
               new TextField(
+                controller: username_controller,
                 decoration: new InputDecoration(
                   hintText: "Type in here!"
                 ),
               
-              onSubmitted: (String str) {
-                name = str;
-                Navigator.of(context).pushNamed("/currentweatherpage");
+              ),
+              RaisedButton(
+                textColor: Colors.white, 
+                color: Colors.blueGrey, 
+                onPressed: () {
+                  String username = username_controller.text;
+
+                  if (username != ""){
+                    print("successful!");
+                    logindata.setBool("login", false);
+
+                    logindata.setString("username", username);
+                    Navigator.of(context).pushNamed("/currentweatherpage");
+                  }
                 }
               )
             ]
@@ -43,11 +80,20 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
+  getData () async{
+    logindata = await SharedPreferences.getInstance();
+    newUser = (logindata.getBool('login') ?? true);
 
-
-getData()async{
-  //SharedPreferences preferences = await SharedPreferences.getInstance();
+    print(newUser);
+      if (newUser == false){
+        Navigator.of(context).pushNamed("/currentweatherpage");
+      }
+    }
+    @override
+  void dispose(){
+    username_controller.dispose();
+    super.dispose();
+  }
 }
 
 class CurrentWeatherPage extends StatefulWidget {
@@ -90,7 +136,7 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
         Center(
           child: Center(child:
            Text (
-            greeting  + " " + name + "                 "+"\n\n" + report + "\n\n\n Do ensure to take an umbrella out today. \n High possibility of rain fall.",
+            greeting  + "\n\n" + report + "\n\n\n Do ensure to take an umbrella out today. \n High possibility of rain fall.",
             textAlign: TextAlign.center,
             style: new TextStyle(fontWeight: FontWeight.bold,
             fontSize: 25.0,
@@ -101,90 +147,3 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
     ));  
   }
 }
-
-/*class SecondPage extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-      children: <Widget>[
-        Image.asset('assets/morning.jpg',
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          fit: BoxFit.cover,
-        ),
-        Center(
-          child: Text("This is a Flutter Example",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28.0,
-                    color: Colors.greenAccent)),
-          ),
-      ],
-    ));  
-  }
-}*/
-
-/*void main() {
-  runApp(new MaterialApp(
-    home: HomePage(),
-    routes: <String, WidgetBuilder> {
-      //"/": (BuildContext context) => new HomePage(),
-      "/secondPage": (BuildContext context) => new SecondPage()
-    }
-  )
-  );
-}
-
-class HomePage extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(title: new Text("Home"), backgroundColor: Colors.deepOrange),
-      body: new Container(
-        child: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new IconButton(
-                icon: new Icon(Icons.favorite, color: Colors.redAccent),
-                iconSize: 70.0,
-                onPressed: () {Navigator.of(context).pushNamed("/secondPage");}
-              ),
-              new Text("Home")
-            ]
-          ),
-        )
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(title: new Text("Second Page"), backgroundColor: Colors.deepOrange),
-      body: new Container(
-        child: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new IconButton(
-                icon: new Icon(Icons.home, color: Colors.blue),
-                iconSize: 70.0,
-                onPressed: null,
-              ),
-              new Text("Second Page")
-            ]
-          ),
-        )
-      ),
-    );
-  }
-}*/
-
-
